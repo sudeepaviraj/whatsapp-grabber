355cf775eaa90eb4233c7705368c50429eca2840
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-07-02 23:05:32 +0530

    tryn to do something better

--- bundle/main.js
+++
@@ -1,8 +0,0 @@
-chrome.action.onClicked.addListener(() => {
-    chrome.tabs.query({active: true, currentWindow: true}).then(([tab]) => {
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        files: ['script.js'],
-      });
-    });
-  });
