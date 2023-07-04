002be6c645436bb1e462f73405250d0d800d3454
Sudeep Virajith <virajithaabekoon@gmail.com>
2023-07-04 20:45:48 +0530

    again

--- index.mjs
+++ index.mjs
@@ -1,11 +1,1 @@
-import {WhatsappClient} from "./functions/WhatsappGrabber.mjs";
-
-const client = new WhatsappClient
-
-const groups = await client.GetGroups()
-
-console.log(groups)
-
-
-
-
+console.log("hi")
